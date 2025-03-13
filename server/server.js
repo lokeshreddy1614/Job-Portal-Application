c57ceaf7578ebbb958ef39a3e139dc002eefbf03
root <root@ip-172-31-37-160.ap-south-1.compute.internal>
2025-03-13 05:38:25 +0000

    Job PortalApplication

--- server/server.js
+++ server/server.js
@@ -1,35 +1,43 @@
 require('dotenv').config();
-const express = require('express')
-const cors = require('cors')
-const authRouter = require('./routes/auth.route')
-const connectDB = require('./db/connectDB')
-const { PORT, CLIENT_URL, NODE_ENV } = require('./configs/config')
-const profileRouter = require('./routes/profile.route')
-const adminRouter = require('./routes/admin.route')
-const applicationRouter = require('./routes/application.route')
-const jobRouter = require('./routes/job.route')
-const cookieParser = require('cookie-parser')
+const express = require('express');
+const cors = require('cors');
+const authRouter = require('./routes/auth.route');
+const connectDB = require('./db/connectDB');
+const { PORT, CLIENT_URL, NODE_ENV } = require('./configs/config');
+const profileRouter = require('./routes/profile.route');
+const adminRouter = require('./routes/admin.route');
+const applicationRouter = require('./routes/application.route');
+const jobRouter = require('./routes/job.route');
+const cookieParser = require('cookie-parser');
 
-const client_url = NODE_ENV === "development" ? "http://localhost:3000" : CLIENT_URL
+const allowedOrigins = [
+    "https://job-portal-application-five.vercel.app",
+    "http://3.111.144.118:3000"
+];
 
-const app = express()
-app.use(express.json())
-app.use(cookieParser())
+const app = express();
+app.use(express.json());
+app.use(cookieParser());
 app.use(cors({
-    origin: client_url,
+    origin: function (origin, callback) {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error("Not allowed by CORS"));
+        }
+    },
     credentials: true,
-}))
+}));
 
-connectDB()
+connectDB();
 
-app.use(express.static("public"))
+app.use(express.static("public"));
 
+app.use('/api/auth', authRouter);
+app.use('/api/profiles', profileRouter);
+app.use('/api/admin', adminRouter);
+app.use('/api/applications', applicationRouter);
+app.use("/api/jobs", jobRouter);
 
-app.use('/api/auth', authRouter)
-app.use('/api/profiles', profileRouter)
-app.use('/api/admin', adminRouter)
-app.use('/api/applications', applicationRouter)
-app.use("/api/jobs", jobRouter)
+app.listen(PORT || 5000, () => console.log(`Server running on port ${PORT || 5000}`));
 
-
-app.listen(PORT || 5000, () => console.log(`Server running on port ${PORT || 5000}`))
